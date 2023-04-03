1bc743b291da4a7754fa09b6fce1c4b2d815a633
Eric Hong <eric.hong@intelicare.com.au>
2023-04-03 15:16:05 +0800

    added entity

--- src/events.controller.ts
+++ src/events.controller.ts
@@ -1,3 +1,4 @@
+import { Event } from './event.entity';
 import { Controller, Delete, Get, Post, Patch } from '@nestjs/common';
 import { Body, HttpCode, Param } from '@nestjs/common/decorators';
 import { CreateEventDto } from './create-event.dto';
@@ -5,28 +6,45 @@ import { UpdateEventDto } from './update-event.dto';
 
 @Controller('/events')
 export class EventsController {
+  private events: Event[] = [];
   @Get()
   findAll() {
-    return [
-      { id: 1, name: '1 event' },
-      { id: 2, name: '2 event' },
-    ];
+    return this.events;
   }
 
   @Get(':id')
   findOne(@Param('id') id) {
+    const event = this.events.find((event) => event.id === parseInt(id));
     return { id: 1, name: '1 event' };
   }
 
   @Post()
   create(@Body() input: CreateEventDto) {
-    return input;
+    const event = {
+      ...input,
+      when: new Date(input.when),
+      id: this.events.length + 1,
+    };
+    this.events.push(event);
+    return event;
   }
 
   @Patch(':id')
-  update(@Param('id') id, @Body() input: UpdateEventDto) {}
+  update(@Param('id') id, @Body() input: UpdateEventDto) {
+    const index = this.events.findIndex((event) => event.id === parseInt(id));
+
+    this.events[index] = {
+      ...this.events[index],
+      ...input,
+      when: input.when ? new Date(input.when) : this.events[index].when,
+    };
+
+    return this.events[index];
+  }
 
   @Delete(':id')
   @HttpCode(204)
-  remove(@Param('id') id) {}
+  remove(@Param('id') id) {
+    this.events = this.events.filter((event) => event.id !== parseInt(id));
+  }
 }
